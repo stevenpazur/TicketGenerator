7c3dbd65f347027d545ef688b76f2c3653ca181e
Steven Pazur <sjpazur@yahoo.com>
2021-05-11 07:16:07 -0700

    added functionality to check for age and isStudent

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace TicketGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int SENIOR_AGE = 65;
+            const int CHILD_AGE = 12;
+
+            int userAge = 20;
+            int ticketPrice = 14;
+
+            bool isStudent = true;
+
+            try {
+                if(userAge < 0){
+                    throw new Exception();
+                }
+
+                if (userAge >= SENIOR_AGE){
+                    ticketPrice = 10;
+                } else if (userAge <= CHILD_AGE){
+                    ticketPrice = 8;
+                }
+
+                Console.WriteLine("Your ticket price is: " + ticketPrice);
+            } catch (Exception e) {
+                Console.WriteLine("Enter a correct age " + e.Message);
+            }
+            
+
+            if(isStudent){
+                ticketPrice = 8;
+            }
+
         }
     }
 }
