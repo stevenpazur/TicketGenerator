58da882d955cd0ce6fe9fa41fc91ec918a092849
Steven Pazur <sjpazur@yahoo.com>
2021-05-14 13:39:27 -0700

    added functionality that updates isStudent based on age then update the price.

--- Program.cs
+++ Program.cs
@@ -4,37 +4,63 @@ namespace TicketGenerator
 {
     class Program
     {
+        const int SENIOR_AGE = 65;
+        const int CHILD_AGE = 12;
+
+        static int userAge = 20;
+        static int ticketPrice = 14;
+
+        static bool isStudent = true;
+
         static void Main(string[] args)
         {
-            const int SENIOR_AGE = 65;
-            const int CHILD_AGE = 12;
+            RunThat();
+        }
 
-            int userAge = 20;
-            int ticketPrice = 14;
+        static void RunThat()
+        {
+            try
+            {
+                Console.WriteLine("Enter your age.");
 
-            bool isStudent = true;
+                string input = Console.ReadLine();
 
-            try {
-                if(userAge < 0){
-                    throw new Exception();
+                if (Int32.TryParse(input, out int value))
+                {
+                    value = Int32.Parse(input);
+                    userAge = value;
+                }
+                else
+                {
+                    throw new Exception("Not a valid number.");
                 }
 
-                if (userAge >= SENIOR_AGE){
+                isStudent = userAge <= CHILD_AGE;
+
+                if (userAge < 0)
+                {
+                    throw new Exception("Enter a non-negative number.");
+                }
+
+                if (userAge >= SENIOR_AGE)
+                {
                     ticketPrice = 10;
-                } else if (userAge <= CHILD_AGE){
+                }
+                else if (userAge <= CHILD_AGE || isStudent)
+                {
                     ticketPrice = 8;
                 }
+                else{
+                    ticketPrice = 14;
+                }
 
-                Console.WriteLine("Your ticket price is: " + ticketPrice);
-            } catch (Exception e) {
-                Console.WriteLine("Enter a correct age " + e.Message);
+                Console.WriteLine("Your ticket price is: $" + ticketPrice);
+                RunThat();
             }
-            
-
-            if(isStudent){
-                ticketPrice = 8;
+            catch (Exception e)
+            {
+                Console.WriteLine("Enter a correct age: " + e.Message);
             }
-
         }
     }
 }
